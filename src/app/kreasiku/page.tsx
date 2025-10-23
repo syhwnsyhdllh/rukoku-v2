75df955467388205b53cc13e0bb7bf36230a7bd7
Syahwan Syahadillah Hamdih <cl09ay@gmail.com>
2025-10-23 22:18:22 +0800

    Created Kreasiku page & Sahabat Peduli Anak page

---
+++ src/app/kreasiku/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import HeroSection from "@/components/HeroSection";
+
+const Kreasiku = () => {
+  return (
+    <HeroSection
+      title="Kreasiku"
+      description="Kumpulan tugas-tugas atau proyek belajar yang diberikan oleh guru di sekolah yang dikerjakan langsung oleh siswa."
+      imageSrc="/images/ilustrasi/Kreasiku.png"
+      imageAlt="Dinas pendidikan"
+    />
+  );
+};
+export default Kreasiku;
---
+++ src/app/sahabat-peduli-anak/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import HeroSection from "@/components/HeroSection";
+
+const SahabatPeduliAnak = () => {
+  return (
+    <HeroSection
+      title="Sahabat Peduli Anak"
+      description="Sahabat Peduli Anak adalah komunitas para ahli yang berbagi waktu dan pengetahuan untuk membantu orang tua dan guru menciptakan pembelajaran yang efektif dan menyenangkan bagi anak."
+      imageSrc="/images/ilustrasi/ahli.png"
+      imageAlt="Dinas pendidikan"
+    />
+  );
+};
+export default SahabatPeduliAnak;
